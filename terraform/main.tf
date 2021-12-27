1d8ccf8290f90b81b9543dea8d1aa81dd2dc0227
sanjay <sanjayntms@yahoo.com>
2021-12-27 10:46:02 +0000

    t2

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
  
 provider "azurerm" {
-  # The "feature" block is required for AzureRM provider 2.x.
+  # The "feature" block is required for AzureRM provider 2.x.ghfgfgf
   # If you're using version 1.x, the "features" block is not allowed.
   version = "~>2.0"
   features {}
